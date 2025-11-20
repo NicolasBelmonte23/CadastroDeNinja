da09de8200c288bbcb2b8423c69c564e97690e5a
Nicolas <Nico.belmonte2014@gmail.com>
2025-11-20 18:54:35 -0300

    Criado a controller aula 2

---
+++ src/main/java/dev/java10x/cadastrodeninja/Controller.java
@@ -0,0 +1,17 @@
+package dev.java10x.cadastrodeninja;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindass(){
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+
+}
