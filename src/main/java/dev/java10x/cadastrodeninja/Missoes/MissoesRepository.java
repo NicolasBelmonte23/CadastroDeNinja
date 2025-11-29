3b66951f3cfeb955238eecfb2b724912d7f80e76
Nicolas <Nico.belmonte2014@gmail.com>
2025-11-29 18:56:42 -0300

    refactor: corrigindo o lugar da interface repository

--- src/main/java/dev/java10x/cadastrodeninja/Missoes/MissoesRepository.java
+++
@@ -1,6 +0,0 @@
-package dev.java10x.cadastrodeninja.Missoes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MissoesRepository extends JpaRepository<MissoesModel,Long> {
-}
